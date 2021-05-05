f956158521071bf341ad30520116b3a40c4d3b5b
Guido Visser <guido-visser@hotmail.com>
2021-05-05 14:54:47 +0200

    some changes to job script

--- job_scripts/train_rec_vae.sh
+++ job_scripts/train_rec_vae.sh
@@ -15,20 +15,18 @@ pip install moviepy
 
 #Copy input file to scratch
 cp -RT $HOME/thesis/datasets/DAVIS_sample_tennis $TMPDIR/data
-cp -RT $HOME/thesis/models/weights/zip_serialization_false/ $TMPDIR/weights
 
 #Create output directory on scratch
 mkdir $TMPDIR/output_dir
 
 #Execute a Python program located in $HOME, that takes an input file and output directory as arguments.
-echo "Start: $(date)" >> $HOME/thesis/job_logs/SeqMaskPropVAE.log
-python $HOME/thesis/train_sequential_VAE_mask_propagation.py \
+echo "Start: $(date)" >> $HOME/thesis/job_logs/RecMaskPropVAE.log
+python $HOME/thesis/train_recurrent_VAE_mask_propagation.py \
             --data_dir $TMPDIR/data \
             --log_dir $TMPDIR/output_dir \
-            --RAFT_weights $TMPDIR/weights/raft-things.pth \
             --epochs 10 \
             --seq_length 6
-echo "End: $(date)" >> $HOME/thesis/job_logs/SeqMaskPropVAE.log
+echo "End: $(date)" >> $HOME/thesis/job_logs/RecMaskPropVAE.log
 
 #Copy output directory from scratch to home
-cp -RT $TMPDIR/output_dir $HOME/thesis/results/SeqMaskPropVAE
+cp -RT $TMPDIR/output_dir $HOME/thesis/results/RecMaskPropVAE
