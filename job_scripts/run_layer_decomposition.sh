19d2133c20008a6ed14ac4e104af418e15a27872
Guido Visser <guido-visser@hotmail.com>
2021-10-18 10:16:21 +0200

    .

--- job_scripts/run_layer_decomposition.sh
+++ job_scripts/run_layer_decomposition.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 #SBATCH -n 1
-#SBATCH -t 12:00:00
+#SBATCH -t 1:00:00
 #SBATCH -p gpu
 #SBATCH --gpus-per-node=gtx1080ti:4
+
+# get start time of script
+DT = date +"%D_%T"
 
 # load modules
 module load 2020
@@ -32,9 +35,9 @@ python $HOME/thesis/run_layer_decomposition.py \
             --propagation_model $TMPDIR/weights/propagation_model.pth \
             --flow_model $TMPDIR/weights/flow_model.pth \
             --batch_size 12 \
-            --n_epochs 510 
+            --n_epochs 1
 echo "End: $(date)" >> $HOME/thesis/job_logs/run_layer_decomposition.log
 
 #Copy output directory from scratch to home
-mkdir $HOME/thesis/results/layer_decomposition/$VIDEO
-cp -RT $TMPDIR/output_dir $HOME/thesis/results/layer_decomposition/$VIDEO
+mkdir -p $HOME/thesis/results/layer_decomposition/$VIDEO/$DT
+cp -RT $TMPDIR/output_dir $HOME/thesis/results/layer_decomposition/$VIDEO/$DT
