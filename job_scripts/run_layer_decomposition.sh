b0a0dfaef6b54032b80104439ccae05fe6f0467e
Guido Visser <guido-visser@hotmail.com>
2021-10-27 15:29:42 +0200

    .

--- job_scripts/run_layer_decomposition.sh
+++ job_scripts/run_layer_decomposition.sh
@@ -39,8 +39,8 @@ python $HOME/thesis/run_layer_decomposition.py \
             --save_freq 100 \
             --mem_freq 2 \
             --alpha_bootstr_thresh 5e-5 \
-            --experiment_config 2 \
-            --description 'Dynamic model with 2001 epochs and high memory frequency. TopkSTM pretrained backbones are used for the memory backbones with channels for object masks included. alpha_bootstrap_threshold is set low. The context is added to the input of the decoder of the reconstruction UNet in the channel dimension. \n    Experiment 2: All object layers (incl. bg layer) share the same memory network.'
+            --experiment_config 3 \
+            --description 'Dynamic model with 2001 epochs and high memory frequency. TopkSTM pretrained backbones are used for the memory backbones with channels for object masks included. alpha_bootstrap_threshold is set low. The context is added to the input of the decoder of the reconstruction UNet in the channel dimension. \n    Experiment 3: Only the bg layer has a memory network. A 1x1 convolutional kernel is used to downsample channels at the point where the context is added.'
 echo "End: $(date)" >> $HOME/thesis/job_logs/run_layer_decomposition.log
 
 #Copy output directory from scratch to home
