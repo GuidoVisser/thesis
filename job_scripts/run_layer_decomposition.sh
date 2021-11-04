67ac426158d1bf35cb4b570f1df6445eb3d947dc
Guido Visser <guido-visser@hotmail.com>
2021-11-04 16:42:34 +0100

    .

--- job_scripts/run_layer_decomposition.sh
+++ job_scripts/run_layer_decomposition.sh
@@ -12,9 +12,9 @@ DT=`date +"%m_%d_%H_%M_%S"`
 module load 2021
 module load Python/3.9.5-GCCcore-10.3.0
 
-# instasll dependencies
+# install dependencies
 pip install --user --upgrade torch && pip install --user --upgrade torchvision
-pip install tensorboard opencv-python
+pip install -r $HOME/thesis/requirement.txt
 
 #Copy input file to scratch
 VIDEO="kruispunt_rijks"
