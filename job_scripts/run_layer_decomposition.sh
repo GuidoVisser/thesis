480e6839fa58097de3871429a733ed354d6fa5ef
Guido Visser <guido-visser@hotmail.com>
2021-11-04 17:31:23 +0100

    .

--- job_scripts/run_layer_decomposition.sh
+++ job_scripts/run_layer_decomposition.sh
@@ -39,8 +39,8 @@ python $HOME/thesis/run_layer_decomposition.py \
             --batch_size 8 \
             --n_epochs 501 \
             --save_freq 100 \
-            --lambda_alpha_l0 0.005 \
-            --lambda_alpha_l1 0.01 \
+            --lambda_alpha_l0 0.015 \
+            --lambda_alpha_l1 0.03 \
             --description 'Dynamic model with 2001 epochs and high memory frequency. TopkSTM pretrained backbones are used for the memory backbones with channels for object masks included. The context is added to the input of the decoder of the reconstruction UNet in the channel dimension.'
 echo "End: $(date)" >> $HOME/thesis/job_logs/run_layer_decomposition.log
 
