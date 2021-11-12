cb9c6248ca9219ecea5a3d47df3533f5246b5ae1
Guido Visser <guido-visser@hotmail.com>
2021-11-12 15:21:54 +0100

    updated job script

--- job_scripts/run_layer_decomposition.sh
+++ job_scripts/run_layer_decomposition.sh
@@ -30,13 +30,13 @@ mkdir $TMPDIR/output_dir
 
 #Execute a Python program located in $HOME, that takes an input file and output directory as arguments.
 echo "Start: $(date)" >> $HOME/thesis/job_logs/run_layer_decomposition.log
-python $HOME/thesis/run_layer_decomposition_3d.py \
+python $HOME/thesis/run_layer_decomposition_2d_3d.py \
             --img_dir $TMPDIR/video \
             --initial_mask $TMPDIR/$MASK_PATH \
             --out_dir $TMPDIR/output_dir \
             --propagation_model $TMPDIR/weights/propagation_model.pth \
             --flow_model $TMPDIR/weights/flow_model.pth \
-            --batch_size 4 \
+            --batch_size 8 \
             --n_epochs 251 \
             --save_freq 50 \
             --conv_channels 64 \
