95454cbd4a3afef21de91b7a3538827a6137bfc7
Guido Visser <guido-visser@hotmail.com>
2021-12-31 14:52:24 +0100

    .

--- update_studies.py
+++ update_studies.py
@@ -1,4 +1,8 @@
 import json
+import pprint as prettyprint
+
+pp = prettyprint.PrettyPrinter(indent=2)
+pprint = pp.pprint
 
 dicts = {}
 
@@ -12,7 +16,22 @@ with open("times.txt") as f:
     data = f.readlines()
 
 for line in data:
-    if len(line == 4):
-        print(line)
-    else:
-        print(line, "XXXX")
+    line = line.strip().split(" ")
+    if len(line) == 4:
+        _, video, identifier, t = line
+
+        study, setting = identifier.split("__")
+    
+        if not "times" in dicts[study]["configs"][setting].keys():
+            dicts[study]["configs"][setting]["times"] = {video: t}
+        else:
+            dicts[study]["configs"][setting]["times"][video] = t
+        
+        if not "times" in dicts[study]["videos"][video].keys():
+            dicts[study]["videos"][video]["times"] = {setting: t}
+        else:
+            dicts[study]["videos"][video]["times"][setting] = t
+
+for study in studies:
+    with open(f"job_scripts/experiments/{study}_updated.json", "w") as f:
+        json.dump(dicts[study], f)
