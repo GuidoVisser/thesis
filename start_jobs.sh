731c5d03d08cac0e8f268d58b08a66e53d0025e0
Guido Visser <guido-visser@hotmail.com>
2021-12-31 14:05:55 +0100

    .

---
+++ start_jobs.sh
@@ -0,0 +1,6 @@
+job_scripts/experiments/3d_conv_study/master.sh
+job_scripts/experiments/alpha_study/master.sh
+job_scripts/experiments/attention_study/master.sh
+job_scripts/experiments/depth_study/master.sh
+job_scripts/experiments/noise_study/master.sh
+job_scripts/experiments/optical_flow_study/master.sh
