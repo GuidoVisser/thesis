ecb36ee9aef4de08c0d88c296dbd0d808933379a
Guido Visser <guido-visser@hotmail.com>
2021-12-31 14:07:41 +0100

    .

--- start_jobs.sh
+++ start_jobs.sh
@@ -1,5 +1,12 @@
 #!/bin/bash 
 
+chmod +x job_scripts/experiments/3d_conv_study/master.sh
+chmod +x job_scripts/experiments/alpha_study/master.sh
+chmod +x job_scripts/experiments/attention_study/master.sh
+chmod +x job_scripts/experiments/depth_study/master.sh
+chmod +x job_scripts/experiments/noise_study/master.sh
+chmod +x job_scripts/experiments/optical_flow_study/master.sh
+
 job_scripts/experiments/3d_conv_study/master.sh
 job_scripts/experiments/alpha_study/master.sh
 job_scripts/experiments/attention_study/master.sh
