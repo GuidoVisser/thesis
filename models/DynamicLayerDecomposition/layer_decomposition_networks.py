e646b5d2c2eea7b353a9547301e2d65856bed6ef
Guido <guido-visser@hotmail.com>
2022-01-28 16:11:57 +0100

    .

--- models/DynamicLayerDecomposition/layer_decomposition_networks.py
+++ models/DynamicLayerDecomposition/layer_decomposition_networks.py
@@ -152,7 +152,7 @@ class LayerDecompositionAttentionMemoryNet(nn.Module):
         for i in range(len(self.context_loader)):
 
             x = self.context_loader[i, layer_idx]
-            print(next(self.encoder.parameters()).device)
+            print(next(self.context_encoder.key_layer.weight.device))
             x = x.to(next(self.encoder.parameters()).device)
 
             with torch.no_grad():
