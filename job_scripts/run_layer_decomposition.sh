baf77ff83a62a5ac0a2427d0db584f408eb0fd58
Guido <guido-visser@hotmail.com>
2022-02-11 09:53:27 +0100

    .

--- job_scripts/run_layer_decomposition.sh
+++ job_scripts/run_layer_decomposition.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 #SBATCH -n 1
-#SBATCH -t 18:00:00
+#SBATCH -t 24:00:00
 #SBATCH -p gpu
 #SBATCH --gpus-per-node=gtx1080ti:4
 
@@ -13,12 +13,12 @@ module load Python/3.8.2-GCCcore-9.3.0
 pip install --user --upgrade torch && pip install --user --upgrade torchvision
 
 #Copy input file to scratch
-VIDEO='raam_kruisend'
+VIDEO='nescio_2'
 cp -RT $HOME/thesis/datasets/Videos/Images/$VIDEO $TMPDIR/video
 mkdir $TMPDIR/00
-cp -RT $HOME/thesis/datasets/Videos/Annotations/$VIDEO/00/00020.png $TMPDIR/00/00020.png
+cp -RT $HOME/thesis/datasets/Videos/Annotations/$VIDEO/00/ $TMPDIR/00/
 mkdir $TMPDIR/01
-cp -RT $HOME/thesis/datasets/Videos/Annotations/$VIDEO/01/00020.png $TMPDIR/01/00020.png
+cp -RT $HOME/thesis/datasets/Videos/Annotations/$VIDEO/01/ $TMPDIR/01/
 mkdir $TMPDIR/weights
 cp $HOME/thesis/models/third_party/weights/topkstm.pth $TMPDIR/weights/propagation_model.pth
 cp $HOME/thesis/models/third_party/weights/raft.pth $TMPDIR/weights/flow_model.pth
@@ -33,7 +33,7 @@ python $HOME/thesis/run_layer_decomposition.py \
             --model_type 3d_bottleneck \
             --device cuda \
             --img_dir $TMPDIR/video \
-            --initial_mask $TMPDIR/00/00006.png $TMPDIR/01/00006.png\
+            --initial_mask $TMPDIR/00/ $TMPDIR/01/ \
             --out_dir $TMPDIR/output_dir \
             --propagation_model $TMPDIR/weights/propagation_model.pth \
             --flow_model $TMPDIR/weights/flow_model.pth \
@@ -45,7 +45,7 @@ python $HOME/thesis/run_layer_decomposition.py \
             --keydim 64 \
             --valdim 256 \
             --timesteps 4 \
-            --num_context_frames 11 \
+            --num_context_frames 9 \
             --lambda_bg_scaling 0.999 \
             --lambda_detail_reg 10 50 0.01 \
             --lambda_mask 1000 50 0 \
