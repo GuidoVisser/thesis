f115c64cc6af358ae20d675897a6f47763c911fb
Guido <guido-visser@hotmail.com>
2022-02-13 12:40:01 +0100

    .

--- job_scripts/run_layer_decomposition.sh
+++ job_scripts/run_layer_decomposition.sh
@@ -46,11 +46,8 @@ python $HOME/thesis/run_layer_decomposition.py \
             --valdim 256 \
             --timesteps 4 \
             --num_context_frames 9 \
-            --corr_diff \
             --use_alpha_detail_reg \
             --lambda_detail_reg 10 50 0.01 \
-            --lambda_dynamics_reg_diff 0.01 \
-            --lambda_dynamics_reg_corr 0.005 \
             --description 'alpha detail reg and corr diff reg'
 
 echo "$SLURM_JOBID | End:   $(date)" >> $HOME/thesis/job_logs/run_layer_decomposition.log
