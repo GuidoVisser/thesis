b319608379a5ba6ab6b5495af083fd4ab0c349a8
Guido <guido-visser@hotmail.com>
2022-02-14 18:50:30 +0100

    .

--- temp.py
+++ temp.py
@@ -4,7 +4,7 @@ def reduce_fr(dirname):
 
     for i, fp in enumerate(sorted(listdir(dirname))):
         if i % 2 == 1:
-            remove(path.join(dirname, fp))
+            # remove(path.join(dirname, fp))
             print(path.join(dirname, fp))
         else:
             idx = i // 2
@@ -12,7 +12,7 @@ def reduce_fr(dirname):
             old = path.join(dirname, fp)
             new = path.join(dirname, f"{idx:05}" + ext)
 
-            rename(old, new)
+            # rename(old, new)
             print(old, new)
 
 def remove_first_n_frames(dirname, n):
@@ -29,17 +29,17 @@ def remove_first_n_frames(dirname, n):
 
 
 if __name__ == "__main__":
-    root = "datasets/Videos"
+    root = "datasets/Experimental"
 
     images = path.join(root, "Images")
     annotations = path.join(root, "Annotations")
 
     videos = listdir(images)
 
-    for video in videos:
-        reduce_fr(path.join(images, video))
-        for layer in listdir(path.join(annotations, video)):
-            reduce_fr(path.join(annotations, video, layer))
+    # for video in videos:
+    #     reduce_fr(path.join(images, video))
+    #     for layer in listdir(path.join(annotations, video)):
+    #         reduce_fr(path.join(annotations, video, layer))
 
     # remove_first_n_frames(path.join(annotations, "amsterdamse_brug/00"), 6)
     # remove_first_n_frames(path.join(annotations, "amsterdamse_brug/01"), 6)
